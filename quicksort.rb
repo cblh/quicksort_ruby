d016e18e6aed463b1654a26831d5f3c2369bf615
baulinchen <905850648@qq.com>
2014-08-13 15:55:41 +0800

    修改来自wiki的方法 使其加入Array类 改为链式调用无需传参

--- quicksort.rb
+++ quicksort.rb
@@ -1,13 +1,3 @@
-# 定义类 加入需传入数组的快排方法 用来检测自己的写的方法对不对
-class QuicksortController
-	def quicksort(array)
-		return array if array.size < 2
-		small, big = array[1..-1].partition { |y| y <= array.first}
-		sorted_array = quicksort(small) + [ array.first ] + quicksort(big)
-		return sorted_array
-	end
-end
-
 # 定义一个数组类 加入快排方法 方便链式调用
 class Array
 	def quick_sort!
@@ -16,12 +6,18 @@ class Array
 	    left, right = a.partition{|t| t < x}
 	    left.quick_sort! + [x] + right.quick_sort!
 	end
+	# 需传入数组的快排方法 from wiki 用来检测自己的写的方法是否正确
+	def quicksort
+		return self if self.size < 2
+		small, big = self[1..-1].partition { |y| y <= self.first}
+		sorted_array = small.quicksort + [ self.first ] + big.quicksort
+		return sorted_array
+	end
 end
 
 # 测试
-qs = QuicksortController.new
-puts qs.quicksort([3, 1, 9, 2, 7, -1]) == [-1, 1, 2, 3, 7, 9]
-puts qs.quicksort([5, -5, 11, 0, 3]) == [-5, 0, 3, 5, 11]
+puts [3, 1, 9, 2, 7, -1].quicksort == [-1, 1, 2, 3, 7, 9]
+puts [5, -5, 11, 0, 3].quicksort == [-5, 0, 3, 5, 11]
 
 b = ((1..100).to_a + (20..80).to_a).shuffle.sample(19)
-puts b.quick_sort! == qs.quicksort(b)
+puts b.quick_sort! == b.quicksort
